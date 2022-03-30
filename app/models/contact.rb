a709b27f1e26033c132d1eece748b9d9c8334717
diego <diegopgcds10@gmail.com>
2022-03-30 15:39:56 -0300

    using I18n for data

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,18 @@
 class Contact < ApplicationRecord
   belongs_to :kind #, optional: true
 
+  # def birthdate_br
+  #   I18n.l(self.birthdate) unless self.birthdate.blank?
+  # end
+
+  def to_br
+    {
+      name: self.name,
+      email: self.email,
+      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?) 
+    }
+  end
+
   # def author
   #   "Diego Silva"
   # end
@@ -16,5 +28,11 @@ class Contact < ApplicationRecord
   #     )
   # end
 
+#   def hello
+#     I18n.t('hello')
+#   end
 
+#   def i18n
+#     I18n.default_locale
+#   end
 end
---
+++ config/initializers/locale.rb
@@ -0,0 +1,3 @@
+# load_path += Dir[Rails.root.join('lib', 'locales', '*.{rb,yml}')]
+
+I18n.default_locale = 'pt-BR'
