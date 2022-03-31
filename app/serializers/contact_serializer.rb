3747cf20e0ca1940a1d18783e84d0f2046f946d1
diego <diegopgcds10@gmail.com>
2022-03-31 12:29:12 -0300

    extra informations on JSON

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,17 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :birthdate
+  attributes :id, :name, :email, :birthdate #, :author
 
   belongs_to :kind
   has_many :phones
   has_one :address
+
+  # def author
+  #   "Diego Silva"
+  # end
+
+  meta do
+     { author: "Diego Silva "}
+  end
 
   def attributes(*args)
     h = super(*args)
