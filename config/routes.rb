ac0b9f1faf80d8e4594015c38ef6be60f60e50b4
diego <diegopgcds10@gmail.com>
2022-03-31 23:15:19 -0300

    query paramenter versioning controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,35 @@
 Rails.application.routes.draw do
   resources :kinds
 
-  resources :contacts do
-    resource :kind, only: [:show]
-    resource :kind, only: [:show], path: 'relationships/kind'
-    
-    resource :phones, only: [:show]
-    resource :phones, only: [:show], path: 'relationships/phones'
-    
-     resource :phone, only: [:update, :create, :destroy]
-    resource :phone , only: [:update, :create, :destroy], path: 'relationships/phones'
-    
-    resource :address, only: [:show, :update, :post, :destroy]
-    resource :address, only: [:show, :update, :post, :destroy], path: 'relationships/address'
+  scope module: 'v1' do
+    resources :contacts, :constraints => lambda { |request| request.params[:version] == "1" } do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
+      
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
+      
+       resource :phone, only: [:update, :create, :destroy]
+      resource :phone , only: [:update, :create, :destroy], path: 'relationships/phones'
+      
+      resource :address, only: [:show, :update, :post, :destroy]
+      resource :address, only: [:show, :update, :post, :destroy], path: 'relationships/address'
+    end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  scope module: 'v2' do
+    resources :contacts, :constraints => lambda { |request| request.params[:version] == "2" } do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
+      
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
+      
+       resource :phone, only: [:update, :create, :destroy]
+      resource :phone , only: [:update, :create, :destroy], path: 'relationships/phones'
+      
+      resource :address, only: [:show, :update, :post, :destroy]
+      resource :address, only: [:show, :update, :post, :destroy], path: 'relationships/address'
+    end
+  end
 end
